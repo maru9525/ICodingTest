6e66caa389033b78482c9d0b124e831a40229e38
maru9525 <cember123@gmail.com>
2022-10-18 23:36:49 +0900

    git test

---
+++ 정훈/10월 1주차/Main_bj_무한문자열_12871.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_bj_무한문자열_12871 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
